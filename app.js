270f402354a4c05a11407b3721a246f0ca26ae72
Nahid Khalili <nahid_khalili@ymail.com>
2022-04-18 17:00:31 +0430

    the base structure of "add skills" designed

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-const startCreateBtton = document.getElementById("create-resume");
+  
+    let skillsArray = [];
+    const addSkillBtn = document.getElementById("confirm-skill-btn");
+    const userInput = document.getElementById("skill-input");
+    const skillList = document.getElementById("skill-list");
 
 
-startCreateBtton.addEventListener("click" , inputValues);
-
-
-function inputValues() {
+function addSkill(){
     
+    const usrInput = userInput.value;
+    if(usrInput.trim() === ''){
+        alert("please put an invalid value");
+    }
+    else{
+        skillsArray.push(usrInput);
+        showResult();
+        clearField();
+    }
 }
+
+function showResult() {
+    const newLi = document.createElement("li");
+    newLi.textContent = `${userInput.value}`;
+    skillList.appendChild(newLi);
+}
+
+function clearField(){
+for(const index of skillsArray){
+    if(index.length != 0){
+        userInput.value = "";
+    }
+}
+}
+addSkillBtn.addEventListener("click", addSkill);
