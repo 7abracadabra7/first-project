bf0f8ae834f37fad939cb263fb4ec81cd719884a
Nahid Khalili <nahid_khalili@ymail.com>
2022-04-25 17:33:16 +0430

    add new skill in result part

--- app.js
+++ app.js
@@ -19,24 +19,47 @@ function addSkill(){
         skillsArray.push(usrInput);
         showResult();
         clearField();
+
     }
 }
 
 function showResult() {
-  
-    const newLi = document.createElement("li");
-    newLi.innerHTML = `
-    <div id="result-modal">
+    const progressArray = document.querySelectorAll('input[name="skill-input-percent"]');
+    console.log(progressArray);
+    let progressAmount; 
+    for(const progress of progressArray){
+        if(progress.checked){
+            progressAmount = progress.value;
+            break;
+        }
+    }
+    const newSkill = document.createElement("div");
+    newSkill.id = "result-modal";
+    newSkill.innerHTML = `
          <div id = "number">
             ${skillCounter}
          </div>
-         <div>
+         <div id="each-result">
+         <div id="result-content">
             ${userInput.value}
          </div>
-    </div>
+         <div id="result-progress">
+         <progress value= ${progressAmount} max="100">
+         </div>
+         </div>
+
+         <div id="edit-box">
+         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" id="edit-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+         <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+         </svg>
+
+         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" id="delete-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+         <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+         </svg>
+         </div>
     `;
     // newLi.textContent = `${userInput.value}`;
-    skillList.appendChild(newLi);
+    skillList.appendChild(newSkill);
     skillCounter++;
 }
 
@@ -53,4 +76,4 @@ function goToNextStep(){
 
 }
 addSkillBtn.addEventListener("click", addSkill);
-nextStepBtn.addEventListener("click", goToNextStep);
+// nextStepBtn.addEventListener("click", goToNextStep);
